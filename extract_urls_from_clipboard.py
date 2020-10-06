0113c20b530f3af86802bc2217082a0231b0e60c
QuantumCalzone <QuantumCalzone@gmail.com>
2020-10-05 22:56:16 -0700

    Create extract_urls_from_clipboard.py

---
+++ extract_urls_from_clipboard.py
@@ -0,0 +1,22 @@
+import clipboard
+import re as regex
+
+everything_before_pattern = regex.compile("^(.*?)http")
+text = clipboard.paste()
+textlines = text.splitlines()
+urls = []
+urls_to_copy = ""
+for textline in textlines:
+    if "http" in textline:
+        urls.append(textline)
+
+for url in urls:
+    to_remove = regex.search(everything_before_pattern, url)
+    url = url.replace(to_remove.group(0), "http")
+    urls_to_copy += url
+    urls_to_copy += "\n"
+    print(url)
+
+
+clipboard.copy(urls_to_copy)
+print("done!")
